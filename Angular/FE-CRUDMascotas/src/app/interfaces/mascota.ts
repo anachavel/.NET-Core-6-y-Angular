64cbe827dd0e9e47e5115fd8b345eb5aaeb7c134
AVANADE-CORP\ana.chavez.velasco <ana.chavez.velasco@gmail.com>
2024-04-22 12:25:17 +0200

    Creación del proyecto y configuración de Angular Material

---
+++ Angular/FE-CRUDMascotas/src/app/interfaces/mascota.ts
@@ -0,0 +1,8 @@
+export interface mascota {
+  id?: number, // Pongo la id opcional porque cuando esté creando una mascota, la base de datos le va a asignar el id automáticamente.
+  nombre: string,
+  edad: number,
+  raza: string,
+  color: string,
+  peso: number
+}
