739aecdeed60e6d7563f4755f527cf8a1bd05ab5
AVANADE-CORP\ana.chavez.velasco <ana.chavez.velasco@gmail.com>
2024-04-22 13:36:25 +0200

    Creación de la tabla para las mascotas

--- Angular/FE-CRUDMascotas/src/app/components/listado-mascota/listado-mascota.component.ts
+++ Angular/FE-CRUDMascotas/src/app/components/listado-mascota/listado-mascota.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Mascota } from 'src/app/interfaces/mascota';
+
+const ELEMENT_DATA: Mascota[] = [
+  { nombre: 'Ciro', edad: 3, raza: 'Golden', color: 'Dorado', peso: 13 },
+  { nombre: 'Kurama', edad: 1, raza: 'Shiba Inu', color: 'Marrón', peso: 5 },
+  { nombre: 'Peluso', edad: 2, raza: 'Pomerania', color: 'Marrón', peso: 3 }
+];
+
 
 @Component({
-  selector: 'app-listado-mascota',
+  selector: 'app-listado-mascota', // Para renderizar este componente, tenemos que utilizar este selector.
   templateUrl: './listado-mascota.component.html',
   styleUrls: ['./listado-mascota.component.css']
 })
 export class ListadoMascotaComponent implements OnInit {
+  displayedColumns: string[] = ['nombre', 'edad', 'raza', 'color', 'peso'];
+  dataSource = ELEMENT_DATA;
 
   constructor() { }
 
--- Angular/FE-CRUDMascotas/src/app/interfaces/mascota.ts
+++ Angular/FE-CRUDMascotas/src/app/interfaces/mascota.ts
@@ -1,4 +1,4 @@
-export interface mascota {
+export interface Mascota {
   id?: number, // Pongo la id opcional porque cuando esté creando una mascota, la base de datos le va a asignar el id automáticamente.
   nombre: string,
   edad: number,
