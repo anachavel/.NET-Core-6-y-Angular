ec89dc0cb36bb8f32d4558a9bbe634b697ca846f
AVANADE-CORP\ana.chavez.velasco <ana.chavez.velasco@gmail.com>
2024-04-24 10:55:34 +0200

    Añado página para poder agregar unsa mascota

--- Angular/FE-CRUDMascotas/src/app/components/agregar-editar-mascota/agregar-editar-mascota.component.ts
+++ Angular/FE-CRUDMascotas/src/app/components/agregar-editar-mascota/agregar-editar-mascota.component.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar-editar-mascota.component.css']
 })
 export class AgregarEditarMascotaComponent implements OnInit {
+  loading: boolean; // Declaro aquí la variable
+  // También puedo ponerlo aquí todo junto: "loading: boolean = false";
+  // NOTA: cuando en el html pongo *ngIf="loading", significa que quiero que se muestre solo cuando, en este caso, "loading" sea true
 
-  constructor() { }
+  constructor() {
+    this.loading = false; // Inicializo aquí la variable
+  }
 
   ngOnInit(): void {
   }
