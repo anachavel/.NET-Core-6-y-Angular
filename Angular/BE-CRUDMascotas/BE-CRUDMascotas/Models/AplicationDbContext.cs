36c3a37092a8905e4d4fe84a88cf37933a6657c7
AVANADE-CORP\ana.chavez.velasco <ana.chavez.velasco@gmail.com>
2024-04-25 22:51:57 +0200

    Carpeta del backend

---
+++ Angular/BE-CRUDMascotas/BE-CRUDMascotas/Models/AplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BE_CRUDMascotas.Models
+{
+    // Esta clase me va a permitir crear la base de datos, acceder a ella, modificar registros, etc.
+    public class AplicationDbContext: DbContext
+    {
+    }
+}
